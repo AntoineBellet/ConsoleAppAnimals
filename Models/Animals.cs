fa0889440952c158f08f45cb27b3013a083ea71e
AntoineBellet <spynetantoine@gmail.com>
2023-03-08 09:28:53 +0100

    build: classe animal avec méthode move et sleep et constructeurs Name

---
+++ Models/Animals.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAnimals.Models
+{
+    class Animal
+    {
+        private string NameAnimal;
+
+        public string Name()
+        {
+            NameAnimal = "Enzo";
+            return NameAnimal;
+        }
+
+        public string Name(string name)
+        {
+            this.NameAnimal = name;
+            return NameAnimal;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine(this.NameAnimal + " Move !");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(this.NameAnimal + " Sleep !");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using ConsoleAppAnimals.Models;
+namespace ConsoleAppAnimals;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        Animal animal1 = new Animal();         
+        Animal animal2 = new Animal();
+
+        animal1.Name();
+        animal2.Name("Bastien");
+        animal1.Move();
+        animal2.Sleep();
+    }
+
+}
